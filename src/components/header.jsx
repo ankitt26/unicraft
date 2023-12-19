579b666de8b5e8cf139c0c09e2354793b4092e58
Ankit <ankit26kushwa@gmail.com>
2023-12-19 22:04:40 +0530

    Add Satoshi font in tailwind.config.js file

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,17 +1,17 @@
 export default () => {
   return (
-    <header className="">
-      <nav className="font-Satoshi flex h-20 flex-row items-center justify-around px-5 pt-5">
-        <h3 className="h-8 w-24 text-center text-2xl font-bold leading-8 tracking-tighter text-gray-700">
+    <header className=" font-Satoshi text-gray-700">
+      <nav className="flex h-20 flex-row items-center justify-around px-5 pt-5">
+        <h3 className="h-[32px] w-[91px] text-center text-2xl font-bold leading-8 tracking-[-0.48px] text-gray-700">
           UniCraft
         </h3>
-        <ul className="flex h-6 flex-row flex-wrap justify-between gap-12 text-lg font-medium tracking-tighter text-[#374151]">
+        <ul className=" flex h-6 flex-row flex-wrap justify-between gap-12 text-lg font-medium tracking-[-0.48px] text-gray-700">
           <li className="h-6 w-auto">How it works</li>
           <li className="h-6 w-auto">Pricing</li>
           <li className="h-6 w-auto">FAQ</li>
         </ul>
-        <button className="h-12 w-36 rounded-lg border-2 border-[#4B5563] px-2 py-2 text-lg font-bold tracking-tighter text-[#4B5563] shadow-inner">
-          Contact Us
+        <button className="h-12 w-36 rounded-lg border-[1px] border-[#4B5563] px-2 py-2 text-lg font-bold tracking-[-0.48px] text-gray-600 shadow-inner">
+          Contact us
         </button>
       </nav>
     </header>
