c16983da54f58def2d30a1b0f79ab04dbb4a39ba
Ankit <ankit26kushwa@gmail.com>
2023-12-20 22:34:12 +0530

    Add boxshadow and animation keyframe in tailwind config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,18 @@ export default {
       fontFamily: {
         Satoshi: ["Satoshi", "sans-serif"],
       },
+      boxShadow: {
+        button: ["2px 4px 10px rgba(202.73, 202.73, 202.73, 0.20) inset"],
+      },
+      animation: {
+        scroll: "scroll 30s linear infinite",
+      },
+      keyframes: {
+        scroll: {
+          "0%": { transform: "translateX(0px)" },
+          "100%": { transform: "translateX(calc(-384px * 4))" },
+        },
+      },
     },
   },
   plugins: [],
