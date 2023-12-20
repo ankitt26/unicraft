8bdedca399445c300a5cb7143039199d4b8f0e69
Ankit <ankit26kushwa@gmail.com>
2023-12-21 03:00:15 +0530

    Create quote section

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Header from "./components/header";
 import { HeroSection } from "./components/herosection";
 import RecentWork from "./components/recentwork";
 import HowItWork from "./components/howitwork";
+import Quote from "./components/quote";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <HeroSection />
       <RecentWork />
       <HowItWork />
+      <Quote />
     </>
   );
 }
