aa819e0a30d80a536082c7e88d6c9ccf611cf5e7
Ankit <ankit26kushwa@gmail.com>
2023-12-21 20:50:57 +0530

    Add screen size in tailwind.config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -18,6 +18,13 @@ export default {
           "100%": { transform: "translateX(calc(-384px * 4))" },
         },
       },
+      screens: {
+        sm: { min: "340px", max: "767px" },
+
+        md: { min: "768px", max: "1023px" },
+
+        lg: { min: "1024px", max: "1279px" },
+      },
     },
   },
   plugins: [],
