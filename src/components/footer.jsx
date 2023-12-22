a4ac3d51892d675fb9a7b94259f195e170825f28
Ankit <ankit26kushwa@gmail.com>
2023-12-22 21:31:37 +0530

    Add footer's Social Icons

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,21 +1,65 @@
+import Twitter from "../assets/socialIcons/Twitter.svg";
+import Linkedin from "../assets/socialIcons/Linkedin.svg";
+import Facebook from "../assets/socialIcons/Facebook.svg";
+import Github from "../assets/socialIcons/Github.svg";
+import Finger from "../assets/socialIcons/Finger.svg";
+import Browser from "../assets/socialIcons/Browser.svg";
 export default function Footer() {
   return (
-    <div className=" bg-Gradiant inline-flex max-h-fit  w-full flex-col items-center justify-start gap-[105px] bg-cover bg-no-repeat ">
-      <div className="mb-10 mt-[195px] flex h-auto w-full flex-col items-center gap-y-7 font-Satoshi">
-        <h2 className="mx-auto w-11/12 break-words text-center font-Satoshi text-7xl font-black leading-[85px] text-gray-800">
-          No long-term contracts.
-          <br /> No catches. Simple.
-        </h2>
-        <p className=" w-4/5 break-words text-center font-Satoshi text-xl font-medium text-gray-600">
-          Start your 30-day free trial. Cancel anytime.
-        </p>
-        <button
-          className="break-words rounded-lg bg-gray-800 px-[51px] py-[16px] font-Satoshi text-xl font-bold text-gray-50 shadow-md"
-          text="View Pricing"
-        >
-          View Pricing
-        </button>
+    <>
+      <div className=" inline-flex max-h-fit w-full  flex-col items-center justify-start gap-[105px] bg-Gradiant bg-cover bg-no-repeat px-[10%] ">
+        <div className="mt-[107px] flex h-auto w-full flex-col items-center gap-y-7 font-Satoshi">
+          <h2 className="mx-auto w-11/12 break-words text-center font-Satoshi text-7xl font-black leading-[85px] text-gray-800">
+            No long-term contracts.
+            <br /> No catches. Simple.
+          </h2>
+          <p className=" mt-3 w-4/5 break-words text-center font-Satoshi text-xl font-medium text-gray-600">
+            Start your 30-day free trial. Cancel anytime.
+          </p>
+          <button
+            className="break-words rounded-lg bg-gray-800 px-[51px] py-[16px] font-Satoshi text-xl font-bold text-gray-50 shadow-md"
+            text="View Pricing"
+          >
+            View Pricing
+          </button>
+        </div>
+
+        {/* social icons */}
+
+        <div className="flex w-full flex-col items-start justify-start border-t-2 border-gray-300 py-[32px]">
+          <h4 className="w-96 text-2xl font-bold text-gray-700">UniCraft</h4>
+          <p className="mt-3 w-96 text-lg font-medium text-gray-400">
+            Design amazing digital experiences that
+            <br />
+            create more happy in the world.
+          </p>
+          <div className="mt-[32px] flex w-full flex-row justify-between">
+            <p className=" w-full font-['Inter'] text-base font-normal leading-normal text-gray-400">
+              © 2077 Untitled UI. All rights reserved.
+            </p>
+            <div className="flex w-full flex-row justify-end gap-6">
+              <a href="#">
+                <img src={Twitter} alt="twitter" />
+              </a>
+              <a href="#">
+                <img src={Linkedin} alt="Linkedin" />
+              </a>
+              <a href="#">
+                <img src={Facebook} alt="Facebook" />
+              </a>
+              <a href="#">
+                <img src={Github} alt="Github" />
+              </a>
+              <a href="#">
+                <img src={Finger} alt="Finger" />
+              </a>
+              <a href="#">
+                <img src={Browser} alt="Browser" />
+              </a>
+            </div>
+          </div>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
